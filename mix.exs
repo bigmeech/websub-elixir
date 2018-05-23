7e5fbf962ff6cff66b23250a13fb027ce7e0055e
Larry Eliemenye <denchural@gmail.com>
2018-05-23 08:06:12 +0100

    add cowboy and plug as main deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Websub.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      applications: [:mongodb, :poolboy],
+      applications: [:mongodb, :poolboy, :cowboy, :plug],
       extra_applications: [:logger]
     ]
   end
