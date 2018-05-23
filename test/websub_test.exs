0d6a78102aadf57ef10c55211bb5b7d5cf0ead27
Larry Eliemenye <denchural@gmail.com>
2018-05-23 07:29:38 +0100

    remove useless tests

--- test/websub_test.exs
+++ test/websub_test.exs
@@ -1,24 +1,4 @@
 defmodule WebsubTest do
   use ExUnit.Case
   doctest Websub
-
-  test "greets the world" do
-    assert Websub.hello() == :world
-  end
-
-  test "add two numbers" do 
-    assert Websub.add(2,4) == 6
-  end
-
-  test "concat two arrays" do 
-    assert Websub.concat([1,2],[3,4]) == [1,2,3,4]
-  end
-
-  test "get head" do 
-    assert Websub.getHead([1,2,3,4,5]) == 1
-  end
-
-  test "get tail" do 
-    assert Websub.getTail([1,2,3,4,5]) == [2,3,4,5]
-  end
 end
